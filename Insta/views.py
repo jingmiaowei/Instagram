8311e1ae978be1d213a9810735c4dd49b7aba4f9
jingmiaowei <jingmiaowei@gmail.com>
2019-07-15 23:41:50 -0500

    Authentication

--- Insta/views.py
+++ Insta/views.py
@@ -2,14 +2,18 @@ from django.views.generic import TemplateView, ListView, DetailView
 from django.views.generic.edit import CreateView,UpdateView,DeleteView
 from Insta.models import Post
 from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from Insta.forms import CustomUserCreationForm
 
 # Create your views here.
 class HelloDjango(TemplateView):
     template_name = 'home.html'
 
-class PostListView(ListView):
+class PostListView(LoginRequiredMixin,ListView):
     model = Post
     template_name = "post.html"
+    login_url = 'login'
 
 class PostDetailView(DetailView):
     model = Post
@@ -29,3 +33,9 @@ class PostDeleteView(DeleteView):
     model = Post
     template_name = "delete_post.html"
     success_url = reverse_lazy ('home')
+
+class SignupView(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = "signup.html"
+    success_url = reverse_lazy ('login')
+
--- InstaDemo/settings.py
+++ InstaDemo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'b#951n!yydes0s&gg9bw8(v4d!xw98h^4pq%_6-z0g+nba0jpk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*’]
 
 
 # Application definition
@@ -122,3 +122,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+LOGIN_REDIRECT_URL = 'home'
+
+LOGOUT_REDIRECT_URL = 'login'
+
+AUTH_USER_MODEL = 'Insta.InstaUser'
