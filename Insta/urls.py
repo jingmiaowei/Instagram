0f2f14f083d8fe883c677d29363251bcac52928b
jingmiaowei <jingmiaowei@gmail.com>
2019-07-16 20:34:01 -0500

    after Ajax

--- Insta/urls.py
+++ Insta/urls.py
@@ -16,14 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
-from Insta.views import PostListView, PostDetailView,PostCreateView, PostUpdateView, PostDeleteView, UserDetail, EditProfile
+from Insta.views import PostListView, PostDetailView,PostCreateView, SignupView, PostUpdateView, PostDeleteView, UserDetail, EditProfile, addLike, addComment
 
 urlpatterns = [
     path('', PostListView.as_view(), name = 'home'),
     path('post/<int:pk>/', PostDetailView.as_view(),name='post_detail'),
     path('make_post/', PostCreateView.as_view(),name='make_post'),
+    path('auth/signup', SignupView.as_view(),name= 'signup'),
     path('update_post/<int:pk>/', PostUpdateView.as_view(),name='update_post'),
     path('delete_post/<int:pk>/', PostDeleteView.as_view(),name='delete_post'),
     path('user/<int:pk>/', UserDetail.as_view(),name='user_profile'),
     path('edit_profile/<int:pk>/', EditProfile.as_view(),name='edit_profile'),
+    path('like', addLike,name='like'),
+    path('comment', addComment,name='comment'),
 ]
--- Insta/views.py
+++ Insta/views.py
@@ -1,10 +1,11 @@
-from django.views.generic import TemplateView, ListView, DetailView
-from django.views.generic.edit import CreateView,UpdateView,DeleteView
-from Insta.models import Post, InstaUser
-from django.urls import reverse_lazy
+from annoying.decorators import ajax_request
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, ListView, TemplateView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from Insta.forms import CustomUserCreationForm
+from Insta.models import InstaUser, Post, UserConnection, Like, Comment
 
 # Create your views here.
 
@@ -45,3 +46,51 @@ class EditProfile(UpdateView):
     model = InstaUser
     template_name = "edit_profile.html"
     fields= ('username','profile_pic',)
+
+@ajax_request
+def addLike(request):
+    post_pk = request.POST.get('post_pk')
+    post = Post.objects.get(pk=post_pk)
+    try:
+        like = Like(post=post, user=request.user)
+        like.save()
+        result = 1
+    except Exception as e:
+        like = Like.objects.get(post=post, user=request.user)
+        like.delete()
+        result = 0
+
+    return {
+        'result': result,
+        'post_pk': post_pk
+    }
+
+
+@ajax_request
+def addComment(request):
+    comment_text = request.POST.get('comment_text')
+    post_pk = request.POST.get('post_pk')
+    post = Post.objects.get(pk=post_pk)
+    commenter_info = {}
+
+    try:
+        comment = Comment(comment=comment_text, user=request.user, post=post)
+        comment.save()
+
+        username = request.user.username
+
+        commenter_info = {
+            'username': username,
+            'comment_text': comment_text
+        }
+
+        result = 1
+    except Exception as e:
+        print(e)
+        result = 0
+
+    return {
+        'result': result,
+        'post_pk': post_pk,
+        'commenter_info': commenter_info
+    }
